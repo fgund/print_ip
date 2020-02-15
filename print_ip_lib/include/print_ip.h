94e799dacace67d7f0a53b81005579edc4b2240b
Filipp Gundelakh <f.gundelakh@yandex.ru>
2020-02-15 12:10:42 +0300

    1) Refactoring, now print_ip only prints and extract_ip returns string. 2) Added template specialization for integers. 3) Added template specialization for containers. 4) Added template specialization for std::string. 5) Added template specialization for std::tuple.

--- print_ip_lib/include/print_ip.h
+++ print_ip_lib/include/print_ip.h
@@ -2,10 +2,12 @@
 #define PRINT_IP_H
 #include <iostream>
 #include <string>
+#include <type_traits>
+
 template<typename T>
-std::string print_ip(T value) {
+typename std::enable_if_t<std::is_integral_v<T>, std::string> extract_ip(const T& value) {
     std::string result;
-    for(int i = sizeof(T)-1; i >= 0; i--){
+    for(int i = sizeof(T)-1; i >= 0; --i){
         result += std::to_string(((value >> (8 * i)) & 0XFF));
         if (i != 0){
             result += ".";
@@ -13,6 +15,56 @@ std::string print_ip(T value) {
     }
     return result;
 }
+
+template<typename T>
+decltype(begin(std::declval<T>()), end(std::declval<T>()), std::string()) extract_ip(const T& container) {
+    std::string result;
+    auto last_elem = end(container);
+    for(auto elem = begin(container); elem != last_elem;){
+        result += std::to_string(*elem);
+        if (++elem != last_elem){
+            result += ".";
+        }
+    }
+    return result;
+}
+
+template<typename...Args>
+std::string extract_ip(const std::tuple<Args...>& t) {
+
+    return TuplePrinter<decltype(t), sizeof...(Args)>::to_string(t);;
+}
+
+
+std::string extract_ip(const std::string& value) {
+    return value;
+}
+
+template <typename T>
+void print_ip(T value){
+    std::cout << extract_ip(value) << std::endl;
+}
+
+template<class Tuple, std::size_t N>
+struct TuplePrinter {
+    static std::string to_string(const Tuple& t)
+    {
+        std::string result;
+        result += TuplePrinter<Tuple, N-1>::to_string(t);
+        result += ".";
+        result += std::to_string(std::get<N-1>(t));
+        return result;
+    }
+};
+
+template<class Tuple>
+struct TuplePrinter<Tuple, 1> {
+    static std::string to_string(const Tuple& t)
+    {
+        return std::to_string(std::get<0>(t));
+    }
+};
+
 
 
 #endif //_PRINT_IP_H
--- src/print_ip_usage.cpp
+++ src/print_ip_usage.cpp
@@ -1,9 +1,16 @@
 #include "print_ip.h"
-#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
 int main()
 {
-    std::cout << print_ip<char>(-1) << std::endl;
-    std::cout << print_ip(int{2130706433}) << std::endl;
-    std::cout << print_ip<long long>(8875824491850138409) << std::endl;
+    print_ip(char{-1});
+    print_ip(short{0});
+    print_ip(int{2130706433});
+    print_ip<long long>(8875824491850138409);
+    print_ip(std::string{"trololo"});
+    print_ip(std::vector{0, 1, 2, 3});
+    print_ip(std::list{10, 20, 30, 40});
+    print_ip(std::make_tuple(0, 1, 2, 3));
     return 0;
 }
