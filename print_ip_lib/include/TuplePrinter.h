d4a30fdde0250974f2db022e0c951eea1a3e42c2
Filipp Gundelakh <f.gundelakh@yandex.ru>
2020-02-15 12:17:19 +0300

    Moved TuplePrinter to independent header.

---
+++ print_ip_lib/include/TuplePrinter.h
@@ -0,0 +1,24 @@
+#ifndef TUPLEPRINTER_H
+#define TUPLEPRINTER_H
+
+template<class Tuple, std::size_t N>
+struct TuplePrinter {
+    static std::string to_string(const Tuple& t)
+    {
+        std::string result;
+        result += TuplePrinter<Tuple, N-1>::to_string(t);
+        result += ".";
+        result += std::to_string(std::get<N-1>(t));
+        return result;
+    }
+};
+
+template<class Tuple>
+struct TuplePrinter<Tuple, 1> {
+    static std::string to_string(const Tuple& t)
+    {
+        return std::to_string(std::get<0>(t));
+    }
+};
+
+#endif //TUPLEPRINTER_H
--- print_ip_lib/include/print_ip.h
+++ print_ip_lib/include/print_ip.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <type_traits>
+#include "TuplePrinter.h"
 
 template<typename T>
 typename std::enable_if_t<std::is_integral_v<T>, std::string> extract_ip(const T& value) {
@@ -44,27 +45,5 @@ template <typename T>
 void print_ip(T value){
     std::cout << extract_ip(value) << std::endl;
 }
-
-template<class Tuple, std::size_t N>
-struct TuplePrinter {
-    static std::string to_string(const Tuple& t)
-    {
-        std::string result;
-        result += TuplePrinter<Tuple, N-1>::to_string(t);
-        result += ".";
-        result += std::to_string(std::get<N-1>(t));
-        return result;
-    }
-};
-
-template<class Tuple>
-struct TuplePrinter<Tuple, 1> {
-    static std::string to_string(const Tuple& t)
-    {
-        return std::to_string(std::get<0>(t));
-    }
-};
-
-
 
 #endif //_PRINT_IP_H
